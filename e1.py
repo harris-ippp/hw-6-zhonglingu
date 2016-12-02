be2f3c37c91a28f09e4481e222f65f5f930cb2d0
Moloq <vmhvilchis@gmail.com>
2016-12-02 15:12:14 -0600

    Graded

--- e1.py
+++ e1.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2015/office_id:1/stage:General"
-req = request.get(url)
+req = request.get(url) ### What is request? you mean requests? 
 html = req.content
 soup = BeautifulSoup(html, "html.parser")
 
@@ -11,3 +11,4 @@ for result in soup.find_all("tr", "election_item"):
     year = result.find("td","year first").contents[0]
     print(id_number,year)
     
+### File output in ELECTION_ID ??
--- e3.py
+++ e3.py
@@ -1,4 +1,5 @@
-import panda as pd
+### Code doesn't execute as given
+import panda as pd ### You mean pandas?
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
@@ -10,7 +11,8 @@ for line in open ("ELECTION_ID"):
     header = pd.read_csv(year + ".csv", nrows = 1).dropna(axis = 1)
     d = header.iloc[0].to_dict()
 
-    df = pd.read_csv(year".csv", index_col = 0,thousands = ",", skiprows = [1])
+### The following line can't even be executed
+    df = pd.read_csv(year+".csv", index_col = 0,thousands = ",", skiprows = [1])
 
     df.rename(inplace = True, columns = d)
     df.dropna(inplace = True, axis = 1)
@@ -28,3 +30,4 @@ for year in range(len(votes)):
 ax  = voteShare.plot(x = 'Year', y = "Republican Vote Share", \
         title = "President General Election Results in Accomack County, Virginia")
 ax. get_figure().savefig("accomack.png")
+### Plot is wrong
